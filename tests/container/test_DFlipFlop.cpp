bbb5cd47ec2648c959a29c3f8ebdc87ae20d1c2f
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-03-01 15:02:49 +0100

    Tests, fix and adapt
    test(container):
    - DFlipFlop tests
    
    bug(container):
    - DFlipFlop fix
    
    impr(chipset):
    - Adapt 4013 chipset

--- tests/container/test_DFlipFlop.cpp
+++ tests/container/test_DFlipFlop.cpp
@@ -7,26 +7,10 @@
 
 #include <criterion/criterion.h>
 
-#include <iostream>
-
 #include "../io/mock/Undefined.hpp"
 #include "container/DFlipFlop.hpp"
 #include "io/False.hpp"
 #include "io/True.hpp"
-
-static std::string stringFromState(nts::Tristate state)
-{
-    switch (state) {
-        case nts::UNDEFINED:
-            return std::string("UNDEFINED");
-        case nts::FALSE:
-            return std::string("FALSE");
-        case nts::TRUE:
-            return std::string("TRUE");
-    }
-
-    return std::string("UNKNOWN");
-}
 
 Test(DFlipFlop, constructor)
 {
@@ -35,13 +19,125 @@ Test(DFlipFlop, constructor)
     delete container;
 }
 
+Test(DFlipFlop, compute_true_false_false_false)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::True();
+    auto *io2 = new io::False();
+    auto *io3 = new io::False();
+    auto *io4 = new io::False();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE; // FALSE
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::FALSE; // TRUE
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_true_false_true_false)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::True();
+    auto *io2 = new io::False();
+    auto *io3 = new io::True();
+    auto *io4 = new io::False();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::FALSE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
 Test(DFlipFlop, compute_false_false_false_false)
 {
     auto *container = new container::DFlipFlop();
-    auto *io1 = new io::True(); // clock
-    auto *io2 = new io::False(); // reset
-    auto *io3 = new io::False(); // data
-    auto *io4 = new io::False(); // set
+    auto *io1 = new io::False();
+    auto *io2 = new io::False();
+    auto *io3 = new io::False();
+    auto *io4 = new io::False();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::UNDEFINED;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::UNDEFINED;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_false_false_true_false)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::False();
+    auto *io2 = new io::False();
+    auto *io3 = new io::True();
+    auto *io4 = new io::False();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::UNDEFINED;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::UNDEFINED;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_false_true_false_false)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::False();
+    auto *io2 = new io::True();
+    auto *io3 = new io::False();
+    auto *io4 = new io::False();
 
     container->setLink(1, *io1, 1);
     container->setLink(2, *io2, 1);
@@ -53,8 +149,313 @@ Test(DFlipFlop, compute_false_false_false_false)
     nts::Tristate actual2 = container->compute(6);
     nts::Tristate expected2 = nts::TRUE;
 
-    std::cout << "actual1: " << stringFromState(actual1) << std::endl;
-    std::cout << "actual2: " << stringFromState(actual2) << std::endl;
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_true_true_false_false)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::True();
+    auto *io2 = new io::True();
+    auto *io3 = new io::False();
+    auto *io4 = new io::False();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::FALSE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::TRUE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_false_true_true_false)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::False();
+    auto *io2 = new io::True();
+    auto *io3 = new io::True();
+    auto *io4 = new io::False();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::FALSE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::TRUE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_true_true_true_false)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::True();
+    auto *io2 = new io::True();
+    auto *io3 = new io::True();
+    auto *io4 = new io::False();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::FALSE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::TRUE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_false_false_false_true)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::False();
+    auto *io2 = new io::False();
+    auto *io3 = new io::False();
+    auto *io4 = new io::True();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::FALSE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_true_false_false_true)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::True();
+    auto *io2 = new io::False();
+    auto *io3 = new io::False();
+    auto *io4 = new io::True();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::FALSE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_false_false_true_true)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::False();
+    auto *io2 = new io::False();
+    auto *io3 = new io::True();
+    auto *io4 = new io::True();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::FALSE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_true_false_true_true)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::True();
+    auto *io2 = new io::False();
+    auto *io3 = new io::True();
+    auto *io4 = new io::True();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::FALSE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_false_true_false_true)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::False();
+    auto *io2 = new io::True();
+    auto *io3 = new io::False();
+    auto *io4 = new io::True();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::TRUE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_true_true_false_true)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::True();
+    auto *io2 = new io::True();
+    auto *io3 = new io::False();
+    auto *io4 = new io::True();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::TRUE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_false_true_true_true)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::False();
+    auto *io2 = new io::True();
+    auto *io3 = new io::True();
+    auto *io4 = new io::True();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::TRUE;
+
+    cr_assert_eq(actual1, expected1);
+    cr_assert_eq(actual2, expected2);
+
+    delete container;
+    delete io1;
+    delete io2;
+    delete io3;
+    delete io4;
+}
+
+Test(DFlipFlop, compute_true_true_true_true)
+{
+    auto *container = new container::DFlipFlop();
+    auto *io1 = new io::True();
+    auto *io2 = new io::True();
+    auto *io3 = new io::True();
+    auto *io4 = new io::True();
+
+    container->setLink(1, *io1, 1);
+    container->setLink(2, *io2, 1);
+    container->setLink(3, *io3, 1);
+    container->setLink(4, *io4, 1);
+
+    nts::Tristate actual1 = container->compute(5);
+    nts::Tristate expected1 = nts::TRUE;
+    nts::Tristate actual2 = container->compute(6);
+    nts::Tristate expected2 = nts::TRUE;
 
     cr_assert_eq(actual1, expected1);
     cr_assert_eq(actual2, expected2);
